e5fe9a7201c01f9d7044f035fc99c1feb57f768e
Pirasanthan Jesugeevegan <pirasanthan.jesugeevegan@CC0092.local>
2023-07-28 11:04:06 +0100

    test

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Get the base image of Node version 16
+FROM node:16
+
+# Get the latest version of Playwright
+FROM mcr.microsoft.com/playwright:focal
+
+# Set up a working directory
+WORKDIR /app
+
+# Set the environment path to node_modules/.bin
+ENV PATH /app/node_modules/.bin:$PATH
+
+# Copy package.json and package-lock.json
+COPY package*.json ./
+
+# Get the needed libraries to run Playwright
+RUN apt-get update && apt-get -y install libnss3 libatk-bridge2.0-0 libdrm-dev libxkbcommon-dev libgbm-dev libasound-dev libatspi2.0-0 libxshmfence-dev
+
+# Install the dependencies in Node environment
+RUN npm install
+# Copy the rest of the application code
+COPY . .
+
+# change permission to execute the script
+RUN chmod +x /entrypoint.sh
+
+# file to execute when the docker container starts up
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+# usage()
+# {
+#     echo "entrypoint.sh:  this script runs cypress, generate a test report and upload it to S3."
+#     echo "" # new line
+#     echo "Params"
+#     echo "--env       What environment you want to run the test on."
+#     echo "-h, --help     Will show this message"
+#     echo "" # new line
+# }
+
+# # defaults
+# env=''
+# # defaults list
+# listOfEnv=("dev" "stage")
+
+# while [ "$1" != "" ]; do
+#     case $1 in
+#         --env )                      
+#             if [[ ! " ${listOfEnv[@]} " =~ " ${2} " ]]; then
+#                 echo "Invalid value for --env. Allowed values are 'dev' or 'stage'."
+#                 exit 1
+#             fi 
+#             env="$2"
+#             ;;
+#         -h | --help )                   
+#             usage
+#             exit
+#             ;;
+#     esac
+#     shift
+# done
+
+
+
+# Display the values of the parameters
+echo "Env: $env"
+
+
+npx playwright test --project=$env
