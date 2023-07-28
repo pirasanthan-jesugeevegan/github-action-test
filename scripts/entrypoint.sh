a6c3d2f933257aec4297a2b6bc45ce8dc2c41cc4
Pirasanthan Jesugeevegan <pirasanthan.jesugeevegan@CC0092.local>
2023-07-28 13:51:59 +0100

    ref

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,45 +1,41 @@
 #!/usr/bin/env bash
 set -e
 
-# usage()
-# {
-#     echo "entrypoint.sh:  this script runs cypress, generate a test report and upload it to S3."
-#     echo "" # new line
-#     echo "Params"
-#     echo "--env       What environment you want to run the test on."
-#     echo "-h, --help     Will show this message"
-#     echo "" # new line
-# }
+usage()
+{
+    echo "entrypoint.sh:  this script runs cypress, generate a test report and upload it to S3."
+    echo "" # new line
+    echo "Params"
+    echo "--env       What environment you want to run the test on."
+    echo "-h, --help     Will show this message"
+    echo "" # new line
+}
 
-# # defaults
-# env=''
-# # defaults list
-# listOfEnv=("dev" "stage")
+# defaults
+env=''
+# defaults list
+listOfEnv=("dev" "stage")
 
-# while [ "$1" != "" ]; do
-#     case $1 in
-#         --env )                      
-#             if [[ ! " ${listOfEnv[@]} " =~ " ${2} " ]]; then
-#                 echo "Invalid value for --env. Allowed values are 'dev' or 'stage'."
-#                 exit 1
-#             fi 
-#             env="$2"
-#             ;;
-#         -h | --help )                   
-#             usage
-#             exit
-#             ;;
-#     esac
-#     shift
-# done
+while [ "$1" != "" ]; do
+    case $1 in
+        --env )                      
+            if [[ ! " ${listOfEnv[@]} " =~ " ${2} " ]]; then
+                echo "Invalid value for --env. Allowed values are 'dev' or 'stage'."
+                exit 1
+            fi 
+            env="$2"
+            ;;
+        -h | --help )                   
+            usage
+            exit
+            ;;
+    esac
+    shift
+done
 
-env_arg="$1"
-
-# Display the value of the --env argument
-echo "Environment: $env_arg"
 
 # Display the values of the parameters
 echo "Env: $env"
-
+npm install
 
 npx playwright test --project=$env
