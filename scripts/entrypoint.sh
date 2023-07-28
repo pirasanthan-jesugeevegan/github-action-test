7eee5beaed919c106ba275568942e0611dd6091f
Pirasanthan Jesugeevegan <pirasanthan.jesugeevegan@CC0092.local>
2023-07-28 14:39:59 +0100

    test

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,43 +1,58 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Environment: $1"
-
 usage()
 {
-    echo "entrypoint.sh:  this script runs cypress, generate a test report and upload it to S3."
+    echo "entrypoint.sh: this script runs cypress, generate a test report and upload it to S3."
     echo "" # new line
     echo "Params"
     echo "--env       What environment you want to run the test on."
-    echo "-h, --help     Will show this message"
+    echo "--type      Select the type of test to run."
+    echo "-h, --help  Will show this message"
     echo "" # new line
 }
 
 # defaults
 env=''
+type=''
+
 # defaults list
 listOfEnv=("dev" "stage")
+listOfType=("api" "ui" "pt")
 
-while [ "$1" != "" ]; do
+while [[ "$#" -gt 0 ]]; do
     case $1 in
         --env )                      
-            if [[ ! " ${listOfEnv[@]} " =~ " ${2} " ]]; then
+            shift
+            if [[ ! " ${listOfEnv[@]} " =~ " ${1} " ]]; then
                 echo "Invalid value for --env. Allowed values are 'dev' or 'stage'."
                 exit 1
             fi 
-            env="$2"
+            env="$1"
+            ;;
+        --type )
+            shift
+            if [[ ! " ${listOfType[@]} " =~ " ${1} " ]]; then
+                echo "Invalid value for --type. Allowed values are 'api', 'ui' or 'pt'."
+                exit 1
+            fi                       
+            type="$1"
             ;;
         -h | --help )                   
             usage
             exit
             ;;
+        * )
+            echo "Unknown option: $1"
+            usage
+            exit 1
+            ;;
     esac
     shift
 done
-
 
 # Display the values of the parameters
 echo "Env: $env"
 
 npm install -D @playwright/test
-npx playwright test --project=$env
+npx playwright test --project="$env"
